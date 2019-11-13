34ec5a47414daefe9cd7a2fb03265c0c4aa9a316
Maria Ntalla <mntalla@pivotal.io>
2019-11-13 14:37:26 +0000

    more wip

--- pkg/crd/markers/topology.go
+++ pkg/crd/markers/topology.go
@@ -17,24 +17,27 @@ limitations under the License.
 package markers
 
 import (
+	"errors"
 	"fmt"
 
+	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"sigs.k8s.io/controller-tools/pkg/markers"
 )
 
-// ToplogyMarkers list topology markers (i.e. markers that specify if a
-// list is an associative-list or a set, or if a map is atomic or not).
+// TopologyMarkers specify topology markers (i.e. markers that describe if a
+// list is an associative-list or a set, if a map is atomic or not).
 var TopologyMarkers = []*definitionWithHelp{
 	must(markers.MakeDefinition("listType", markers.DescribesField, ListType(""))),
 	must(markers.MakeDefinition("listMapKey", markers.DescribesField, ListMapKey(""))),
+	must(markers.MakeDefinition("mapType", markers.DescribesField, MapType(""))),
 }
 
 func init() {
 	AllDefinitions = append(AllDefinitions, TopologyMarkers...)
 }
 
-// +controllertools:marker:generateHelp:category="CRD topology"
+// +controllertools:marker:generateHelp:category="CRD processing"
 // ListType specifies the type of data-structure that the list
 // represents (map, set, atomic).
 //
@@ -48,7 +51,7 @@ func init() {
 // are typically manipulated together by the same actor.
 type ListType string
 
-// +controllertools:marker:generateHelp:category="CRD topology"
+// +controllertools:marker:generateHelp:category="CRD processing"
 // ListMapKey specifies the keys to map listTypes.
 //
 // ListMapKey indicates the index of a map list. They can be repeated if
@@ -56,11 +59,26 @@ type ListType string
 // to map, and the keys should be scalar types.
 type ListMapKey string
 
+// +controllertools:marker:generateHelp:category="CRD processing"
+
+// MapType specifies the level of atomicity of the map,
+// i.e. whether each item in the map is independent of the others,
+// or all fields are treated as a single unit.
+//
+// Possible values:
+// - "granular": items in the map are independent of each other,
+// and can be manipulated by different actors.
+// This is the default behavior.
+// - "atomic": all fields are treated as one unit.
+// Any changes have to replace the entire map.
+
+type MapType string
+
 func (l ListType) ApplyToSchema(schema *v1beta1.JSONSchemaProps) error {
 	if schema.Type != "array" {
 		return fmt.Errorf("must apply listType to an array")
 	}
-	if l != "map" || l != "atomic" || l != "set" {
+	if l != "map" && l != "atomic" && l != "set" {
 		return fmt.Errorf(`ListType must be either "map", "set" or "atomic"`)
 	}
 	p := string(l)
@@ -80,3 +98,22 @@ func (l ListMapKey) ApplyToSchema(schema *v1beta1.JSONSchemaProps) error {
 	schema.XListMapKeys = append(schema.XListMapKeys, string(l))
 	return nil
 }
+
+func (m MapType) ApplyToSchema(schema *apiext.JSONSchemaProps) error {
+	// if schema.Type != "map" {
+	// 	//TODO error; this only applies to maps
+	// }
+	//
+	// if m != "one of atomic or granular" {
+	// 	//TODO error; only atomic/granular are valid options
+	// }
+	//
+	// //otherwise apply to schema
+	// p := string(m)
+	// schema.XMapType = &p
+
+	return errors.New("not implemented")
+}
+
+//TODO test cover
+func (l MapType) ApplyFirst() {}
